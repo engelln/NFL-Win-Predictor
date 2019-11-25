898702b28dc73a5e7e70523c9f5a95c181981a08
Nick Engell <engelln@go.stockton.edu>
2019-11-24 23:17:15 -0500

    Testing Sunday Night game

--- Main/Predictor.py
+++ Main/Predictor.py
@@ -4,19 +4,19 @@ import numpy as np
 
 # Editable Parameters vvvvv
 model_dir = "D:\\Data\\NFL\\output\\model.txt"
-home = "NYG"
-away = "CHI"
+home = "SF"
+away = "GB"
 ht_pos = True
-score = (0, 0)  # home, away
-timeouts = (3, 3)  # home, away
-qtr = 1
-time = (15, 0)  # mins, secs
+score = (23,8)  # home, away
+timeouts = (3, 2)  # home, away
+qtr = 3
+time = (3, 4)  # mins, secs
 down_info = (1, 10)  # down x, y yards to first down
-ydstotd = 75
+ydstotd = 60
 week = 12
 season = 2019
-htst = (3,7,0)  # W, L, T
-atst = (3,7,0)
+htst = (9,1,0)  # W, L, T
+atst = (8,2,0)
 # Editable Parameters ^^^^^
 
 
